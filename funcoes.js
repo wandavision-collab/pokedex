4666b5007332e64d7668a493cc41e67e0283f91c
wandavision-collab <82836697+wandavision-collab@users.noreply.github.com>
2022-04-06 16:08:34 -0300

    pokedex

--- funcoes.js
+++ funcoes.js
@@ -15,10 +15,22 @@ const fetchPokemon = () => {
     Promise.all(pokemonPromises)
         .then(pokemons => {
             pokemons.forEach(pokemon => {
+                let tipo = pokemon.types[0].type.name
+                let id_imagem
+                if(pokemon.id < 10){
+                    id_imagem = "00" + pokemon.id
+                }else if(pokemon.id < 100){
+                    id_imagem = "0" + pokemon.id
+
+                }else{
+                    id_imagem = pokemon.id
+                }
+              
                 html += `
-                    <li class="cartao">
+                    <li class="cartao ${tipo}">
                         <h2 class="cartao-titulo">${pokemon.id} - ${pokemon.name}</h2>
-                        <img src="${pokemon.sprites.front_default}">
+                        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id_imagem}.png">
+                        <h3 class="cartao-subtitulo">${tipo}</h3>
                     </li> 
                 `
             });
